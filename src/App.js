e05ef8739a4de4fe0e8b669ae45e719b356a95e0
AnnieSoos <anniesoos007@gmail.com>
2018-02-22 14:17:52 +0100

    new css file and testfile

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <Title content = "Hello world"/>
+        <Title content="Scoreboard" />
+      
+        </div>
     );
   }
 }
 
 export default App;
+
+
+
+//<div className="App">
+//  <header className="App-header">
+//    <img src={logo} className="App-logo" alt="logo" />
+//    <h1 className="App-title">Welcome to React</h1>
+//  </header>
+//  <p className="App-intro">
+//    To get started, edit <code>src/App.js</code> and save to reload.
+//  </p>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import Title from './components/Title'
+
+
+describe('<App />', () => {
+  const app = shallow(<App />)
+  const title = <Title content="Scoreboard" />
+
+  it('wraps everything in a div tag', () => {
+    expect(app).toHaveTagName('div')
+  })
+
+  it('contains a Title', () => {
+    expect(app).toContainReact(title)
+  })
+})
